27d0a9cf3b9f5c012544f3a7bd8fb0aca38a0bcf
grey29boss <52293468+grey29boss@users.noreply.github.com>
2021-10-09 15:17:50 +0700

    Update bosov_1-3-9.py

--- bosov_1-3-9.py
+++ bosov_1-3-9.py
@@ -3,28 +3,9 @@ with open ("str_1.txt") as f:# чтение из файла
     for line in f:
         line = line.rstrip().split()#тут сделал список
         print(line)
-        #print(len(line))
+        
 s=line[::-1]#список последний элемент на первом месте
 print(str(s))
 with open ("new.txt","w") as file:#запись списка в файл в строчку, потому что ' ' пробел, а если было '\n' , то каждый элемент сновой строки
     for items in s:
         file.write(items+' ')
-
-
-
-#with open('dataset.txt') as inf:
-    #for line in inf:
-        #line= line.replace('\n')
-        #line=line.strip().lower().splitlines()
-       #print(line)
-#def create_dict(list_of_words):#создание словаря из списка строк- функция
-    #d = {}
-    #for word in list_of_words:
-        #if word not in d:
-            #d[word]=1
-        #else:
-            #d[word]+=1
-    #return d
-
-#sl = create_dict(line)
-#print(sl)
